6b6bd8dd45d1020f2217f19626473f587930e7a7
Bolormaa <135795277+bolourima@users.noreply.github.com>
2023-12-01 19:36:09 +0800

    done

--- todo.js
+++ todo.js
@@ -1,6 +1,10 @@
 let addCartbtn = document.querySelectorAll(".addCard");
-// console.log(addCartbtn, "add card button");
 let addTaskBox = document.querySelector(".addTask");
+let titleTextInput = document.getElementById("titleInput");
+let destTextInput = document.getElementById("descriptionInput");
+let statusSelectRef = document.getElementById("status-select");
+let prioritySelectRef = document.getElementById("priority-select");
+let taskCounttext = document.getElementById("taskCounttext");
 
 // Add Task -ийг ил харагдуулах функц
 let currentStatus = "todo";
@@ -49,7 +53,10 @@ function render() {
     const card = document.createElement("div");
     card.className = "card";
 
-    const checkBtn = `<button class="checkBtn">ok</button>`;
+    const checkBtn = document.createElement("button");
+    checkBtn.className = "checkBtn";
+    checkBtn.innerHTML = "ok";
+    checkBtn.id = task.id;
 
     const titDescDiv = document.createElement("div");
     const taskTitle = document.createElement("p");
@@ -61,39 +68,65 @@ function render() {
 
     const twoButtDiv = document.createElement("div");
     twoButtDiv.className = "twoButtDiv";
-    const deleteBtn = `<button class="deleteBtn">x</button>`;
-    const editBtn = `<button class="editBtn">edit</button>`;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.innerHTML = "x";
+    deleteBtn.id = task.id;
 
-    const priorityDad = document.createElement("div");
-    priorityDad.className = "priorityDad"
+    const editBtn = document.createElement("button");
+    editBtn.className = "editBtn";
+    editBtn.innerHTML = "edit";
+    editBtn.id = task.id;
+
     const priorityDiv = document.createElement("div");
     priorityDiv.className = "priorityDiv";
     priorityDiv.textContent = task.priority;
-    console.log(task.priority);
 
     cardTodo.appendChild(card);
-    card.innerHTML += checkBtn;
+
+    deleteBtn.onclick = function (event) {
+      let taskIdToDelete = event.target.id;
+      // console.log(taskIdToDelete);
+      state.tasks = state.tasks.filter((task) => task.id !== taskIdToDelete);
+      render();
+    };
+    editBtn.onclick = function (event) {
+      writenTextTitle.value = taskTitle.textContent;
+      writenDescInput.value = taskDesc.textContent;
+      taskId.value = task.id;
+      let taskIdToDelete = event.target.id;
+      state.tasks = state.tasks.filter((task) => task.id !== taskIdToDelete);
+      addTaskBox.style.display = "flex";
+      addTaskBox.style.flexDirection = "column";
+      task.text = "";
+      task.title = "";
+    };
+
+    checkBtn.onclick = function (event) {
+      let taskIdtoDone = event.target.id;
+      state.tasks = state.tasks.map((task) => {
+        if (task.id === taskIdtoDone) {
+          return { ...task, status: "done" };
+        }
+      });
+      render();
+    };
+    function taskCount() {
+      let tasknum = cardTodo.childElementCount;
+      taskCounttext.textContent = tasknum;
+      console.log("tasknum", tasknum);
+      // console.log("taskcounttext", taskCounttext);
+    }
+    taskCount();
+
+    card.appendChild(checkBtn);
     card.appendChild(titDescDiv);
     titDescDiv.appendChild(taskTitle);
     titDescDiv.appendChild(taskDesc);
+    titDescDiv.appendChild(priorityDiv);
     card.appendChild(twoButtDiv);
-    twoButtDiv.innerHTML += deleteBtn;
-    twoButtDiv.innerHTML += editBtn;
-    cardTodo.appendChild(priorityDad);
-    priorityDad.appendChild(priorityDiv)
-
-    const check = document.querySelector(".checkBtn");
-    check.addEventListener("click", () => {
-      cardDone.appendChild(cardTodo);
-    });
-
-
-    // const deleteB = document.querySelector(".deleteBtn");
-
-    // deleteB.addEventListener("click", () => {
-    //   cardTodo.remove();
-    //   // array find filter(task => task.id !== id)
-    // });
+    twoButtDiv.appendChild(deleteBtn);
+    twoButtDiv.appendChild(editBtn);
   });
 
   // On the In Progress card add task function
@@ -101,7 +134,10 @@ function render() {
     const card = document.createElement("div");
     card.className = "card";
 
-    const checkBtn = `<button class="checkBtn">ok</button>`;
+    const checkBtn = document.createElement("button");
+    checkBtn.className = "checkBtn";
+    checkBtn.innerHTML = "ok";
+    checkBtn.id = task.id;
 
     const titDescDiv = document.createElement("div");
     const taskTitle = document.createElement("p");
@@ -113,17 +149,58 @@ function render() {
 
     const twoButtDiv = document.createElement("div");
     twoButtDiv.className = "twoButtDiv";
-    const deleteBtn = `<button class="deleteBtn">x</button>`;
-    const editBtn = `<button class="editBtn">edit</button>`;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.innerHTML = "x";
+    deleteBtn.id = task.id;
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "editBtn";
+    editBtn.innerHTML = "edit";
+    editBtn.id = task.id;
+
+    const priorityDiv = document.createElement("div");
+    priorityDiv.className = "priorityDiv";
+    priorityDiv.textContent = task.priority;
 
     cardInProgress.appendChild(card);
-    card.innerHTML += checkBtn;
+
+    deleteBtn.onclick = function (event) {
+      let taskIdToDelete = event.target.id;
+      // console.log(taskIdToDelete);
+      state.tasks = state.tasks.filter((task) => task.id !== taskIdToDelete);
+      render();
+    };
+    editBtn.onclick = function (event) {
+      writenTextTitle.value = taskTitle.textContent;
+      writenDescInput.value = taskDesc.textContent;
+      taskId.value = task.id;
+      let taskIdToDelete = event.target.id;
+      state.tasks = state.tasks.filter((task) => task.id !== taskIdToDelete);
+      addTaskBox.style.display = "flex";
+      addTaskBox.style.flexDirection = "column";
+      task.text = "";
+      task.title = "";
+    };
+
+    checkBtn.onclick = function (event) {
+      let taskIdtoDone = event.target.id;
+      state.tasks = state.tasks.map((task) => {
+        if (task.id === taskIdtoDone) {
+          return { ...task, status: "done" };
+        }
+      });
+      render();
+    };
+
+    card.appendChild(checkBtn);
     card.appendChild(titDescDiv);
     titDescDiv.appendChild(taskTitle);
     titDescDiv.appendChild(taskDesc);
+    titDescDiv.appendChild(priorityDiv);
     card.appendChild(twoButtDiv);
-    twoButtDiv.innerHTML += deleteBtn;
-    twoButtDiv.innerHTML += editBtn;
+    twoButtDiv.appendChild(deleteBtn);
+    twoButtDiv.appendChild(editBtn);
   });
 
   // On the Stuck card add task function
@@ -131,7 +208,10 @@ function render() {
     const card = document.createElement("div");
     card.className = "card";
 
-    const checkBtn = `<button class="checkBtn">ok</button>`;
+    const checkBtn = document.createElement("button");
+    checkBtn.className = "checkBtn";
+    checkBtn.innerHTML = "ok";
+    checkBtn.id = task.id;
 
     const titDescDiv = document.createElement("div");
     const taskTitle = document.createElement("p");
@@ -143,17 +223,58 @@ function render() {
 
     const twoButtDiv = document.createElement("div");
     twoButtDiv.className = "twoButtDiv";
-    const deleteBtn = `<button class="deleteBtn">x</button>`;
-    const editBtn = `<button class="editBtn">edit</button>`;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.innerHTML = "x";
+    deleteBtn.id = task.id;
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "editBtn";
+    editBtn.innerHTML = "edit";
+    editBtn.id = task.id;
+
+    const priorityDiv = document.createElement("div");
+    priorityDiv.className = "priorityDiv";
+    priorityDiv.textContent = task.priority;
 
     cardStuck.appendChild(card);
-    card.innerHTML += checkBtn;
+
+    deleteBtn.onclick = function (event) {
+      let taskIdToDelete = event.target.id;
+      // console.log(taskIdToDelete);
+      state.tasks = state.tasks.filter((task) => task.id !== taskIdToDelete);
+      render();
+    };
+    editBtn.onclick = function (event) {
+      writenTextTitle.value = taskTitle.textContent;
+      writenDescInput.value = taskDesc.textContent;
+      taskId.value = task.id;
+      let taskIdToDelete = event.target.id;
+      state.tasks = state.tasks.filter((task) => task.id !== taskIdToDelete);
+      addTaskBox.style.display = "flex";
+      addTaskBox.style.flexDirection = "column";
+      task.text = "";
+      task.title = "";
+    };
+
+    checkBtn.onclick = function (event) {
+      let taskIdtoDone = event.target.id;
+      state.tasks = state.tasks.map((task) => {
+        if (task.id === taskIdtoDone) {
+          return { ...task, status: "done" };
+        }
+      });
+      render();
+    };
+
+    card.appendChild(checkBtn);
     card.appendChild(titDescDiv);
     titDescDiv.appendChild(taskTitle);
     titDescDiv.appendChild(taskDesc);
+    titDescDiv.appendChild(priorityDiv);
     card.appendChild(twoButtDiv);
-    twoButtDiv.innerHTML += deleteBtn;
-    twoButtDiv.innerHTML += editBtn;
+    twoButtDiv.appendChild(deleteBtn);
+    twoButtDiv.appendChild(editBtn);
   });
 
   // On the Done card add task function
@@ -161,7 +282,10 @@ function render() {
     const card = document.createElement("div");
     card.className = "card";
 
-    const checkBtn = `<button class="checkBtn">ok</button>`;
+    const checkBtn = document.createElement("button");
+    checkBtn.className = "checkBtn";
+    checkBtn.innerHTML = "ok";
+    checkBtn.id = task.id;
 
     const titDescDiv = document.createElement("div");
     const taskTitle = document.createElement("p");
@@ -173,31 +297,69 @@ function render() {
 
     const twoButtDiv = document.createElement("div");
     twoButtDiv.className = "twoButtDiv";
-    const deleteBtn = `<button class="deleteBtn">x</button>`;
-    const editBtn = `<button class="editBtn">edit</button>`;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "deleteBtn";
+    deleteBtn.innerHTML = "x";
+    deleteBtn.id = task.id;
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "editBtn";
+    editBtn.innerHTML = "edit";
+    editBtn.id = task.id;
+
+    const priorityDiv = document.createElement("div");
+    priorityDiv.className = "priorityDiv";
+    priorityDiv.textContent = task.priority;
 
     cardDone.appendChild(card);
-    card.innerHTML += checkBtn;
+
+    deleteBtn.onclick = function (event) {
+      let taskIdToDelete = event.target.id;
+      // console.log(taskIdToDelete);
+      state.tasks = state.tasks.filter((task) => task.id !== taskIdToDelete);
+      render();
+    };
+    editBtn.onclick = function (event) {
+      writenTextTitle.value = taskTitle.textContent;
+      writenDescInput.value = taskDesc.textContent;
+      taskId.value = task.id;
+      let taskIdToDelete = event.target.id;
+      state.tasks = state.tasks.filter((task) => task.id !== taskIdToDelete);
+      addTaskBox.style.display = "flex";
+      addTaskBox.style.flexDirection = "column";
+      task.text = "";
+      task.title = "";
+    };
+
+    checkBtn.onclick = function (event) {
+      let taskIdtoDone = event.target.id;
+      state.tasks = state.tasks.map((task) => {
+        if (task.id === taskIdtoDone) {
+          return { ...task, status: "done" };
+        }
+      });
+      render();
+    };
+
+    card.appendChild(checkBtn);
     card.appendChild(titDescDiv);
     titDescDiv.appendChild(taskTitle);
     titDescDiv.appendChild(taskDesc);
+    titDescDiv.appendChild(priorityDiv);
     card.appendChild(twoButtDiv);
-    twoButtDiv.innerHTML += deleteBtn;
-    twoButtDiv.innerHTML += editBtn;
+    twoButtDiv.appendChild(deleteBtn);
+    twoButtDiv.appendChild(editBtn);
   });
 }
-
 // Add task Function
-
+let writenTextTitle;
+let writenDescInput;
+let taskId;
 function addTaskFunc(event, currentStatus) {
   // console.log(event.target.id);
-  let titleTextInput = document.getElementById("titleInput");
-  let destTextInput = document.getElementById("descriptionInput");
-  let statusSelectRef = document.getElementById("status-select");
-  let prioritySelectRef = document.getElementById("priority-select");
-
-  let writenTextTitle = titleTextInput.value.trim();
-  let writenDescInput = destTextInput.value.trim();
+  writenTextTitle = titleTextInput.value.trim();
+  writenDescInput = destTextInput.value.trim();
+  taskId = Math.random().toString(16).slice(2);
 
   if (writenTextTitle !== "" && destTextInput !== "") {
     state.tasks.push({
@@ -205,18 +367,13 @@ function addTaskFunc(event, currentStatus) {
       text: writenDescInput,
       status: statusSelectRef.value || currentStatus,
       priority: prioritySelectRef.value,
+      id: taskId,
     });
-
     render();
-
-    // add task box -iig haragduulahgui bolgoh
+    titleTextInput.value = "";
+    destTextInput.value = "";
+ 
+    
     addTaskBox.style.display = "none";
   }
 }
-
-// when click Check button card go to the done card
-
-// function checkFunc() {
-//   cardDone.appendChild(card);
-// }
-// checkBtn.addEventListener("click", checkFunc);
